852e3faef84477ba5155fec2350fcddc8d9e5157
boltonlin <boltonlinbo@gmail.com>
2022-12-25 10:53:03 -0800

    added methods

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -5,21 +5,27 @@
 var Rooms = {
 
   // TODO: Define how you want to store the list of rooms
-  list: {},
+  _list: {},
+  _current: undefined,
 
   // TODO: Define methods which allow you to add rooms, update the list,
   // mark a room as selected, etc.
 
   add: function (roomname) {
-    Rooms.list[roomname] = roomname;
+    Rooms._list[roomname] = roomname;
   },
 
-  get: function (roomname) {
-    return Rooms.list[roomname];
+  // gets current room
+  get: function () {
+    return Rooms._current;
+  },
+
+  set: function (roomname) {
+    Rooms._current = roomname;
   },
 
   exists: function (roomname) {
-    if (Rooms.get(roomname)) return true;
+    if (Rooms._list[roomname]) return true;
     else return false;
   },
 
