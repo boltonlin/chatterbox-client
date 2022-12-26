32e3f433e04fd2033a33b76a4b6ac9bf9159bdef
boltonlin <boltonlinbo@gmail.com>
2022-12-25 18:02:54 -0800

    cleaned

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -5,16 +5,16 @@
 var Friends = {
   // TODO: Define how you want to store your list of friends.
 
-  list: {},
+  _list: {},
 
   // TODO: Define methods which allow you to add, toggle,
   // and check the friendship status of other users.
 
   toggleStatus: function (username) {
-    if (!!Friends.list[username]) {
-      delete Friends.list[username];
+    if (!!Friends._list[username]) {
+      delete Friends._list[username];
     } else {
-      Friends.list[username] = username;
+      Friends._list[username] = username;
     }
   },
 
